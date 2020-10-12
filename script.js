2b92f6888f23b2909e9cc14c225a3bdb529c62d9
Dinesh Sadhwani <dineshsadhwani4@gmail.com>
2020-10-13 03:20:49 +0530

    deploy app

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('Testing');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt user to select a media stream, pass it to video element, play
+
+const selectMediaStream = async() => {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+button.addEventListener('click', async() => {
+  // Disable the button
+  button.disabled = true;
+  // Start PiP
+  await videoElement.requestPictureInPicture();
+  button.disabled = false;
+});
+
+
+// On Load
+selectMediaStream();
